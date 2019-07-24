24b755e9b678b13d27b84c714d71fb985166a0ee
chernig <cherno_9@mail.ru>
2019-07-24 21:54:55 +1000

    Done

--- data_splitter.py
+++ data_splitter.py
@@ -1,31 +1,54 @@
 import os
 import glob
+import shutil
+import random
 
 print('Would you like to have a validation folder? y/n')
 answer = input()
 folders = []
 types = ['*.jpg', '*.jpeg', '*.png']
 counter = 0
-
+files_lst = []
+path = os.getcwd()
+folders.extend(['training', 'test'])
 if answer =='y':
+    folders.append('validation')
     print('Please enter the desired propotion betweet training / test / validation sets (for example 0.8 0.1 0.1):')
     training_scale, test_scale, valid_scale = map(float, input().split())
-    os.mkdir('training')
-    os.mkdir('test')
-    os.mkdir('validation')
 else:
-    print('Please enter the desired propotion betweet training / test / (for example 0.8/0.2):')
-    training_scale, test_scale = input().split('/')
-    os.mkdir('training')
-    os.mkdir('test')
+    print('Please enter the desired propotion betweet training / test (for example 0.8 0.2):')
+    training_scale, test_scale = map(float, input().split())
+for x in folders:
+    os.mkdir(x)
 
-#Count all images in folder
+#Count all images in folder and add them into list
 for image_type in types:
-    for _ in glob.glob(image_type):
+    for x in glob.glob(image_type):
+        files_lst.append(x)
         counter+=1
 
+# Scale to actual numbers
 training_files = int(training_scale*counter)
 test_files = int(test_scale*counter)
-validation_files = counter - training_files - test_files
+i = 0
 
-print(training_files, test_files, validation_files)
+# Create necessary paths
+train_path = path + '\\training'
+test_path = path +'\\test'
+
+# Copy to training folder
+while i < training_files:
+    i+=1
+    shutil.copy(files_lst.pop(random.randint(0 , len(files_lst)-1)), train_path)
+i = 0
+
+# Copy to test folder
+while i < test_files:
+    i+=1
+    shutil.copy(files_lst.pop(random.randint(0 , len(files_lst)-1)), test_path)
+# Copy to validation folder if applicable
+if 'valid_scale' in locals():
+    valid_path = path + '\\validation'
+    for x in files_lst:
+        shutil.copy(x, valid_path)
+
